16de87e35d9c132605b7b528e5849fdba1225a28
deepaksinghmudila <deepaksinghmudila21@gmail.com>
2023-08-19 22:28:02 +0530

    handled post request

--- home/models.py
+++ home/models.py
@@ -8,3 +8,7 @@ class Contact(models.Model):
     phone = models.CharField(max_length=13)
     content = models.TextField()
     timeStamp = models.DateTimeField(auto_now_add=True, blank=True)
+
+    def __str__(self):
+        return 'Message from ' + self.name + '- ' + self.email
+
