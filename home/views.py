972467ccf233f780140e95ec6573e889f1f14ecd
deepaksinghmudila <deepaksinghmudila21@gmail.com>
2023-09-05 22:39:21 +0530

    home page updation

--- home/views.py
+++ home/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib import messages
+
 from home.models import Contact
+
 
 # Create your views here.
 def home(request):
@@ -8,8 +11,10 @@ def home(request):
     # return HttpResponse('Inside Home Page!')
 
 def about(request):
+    messages.error(request, 'This is About')
+    messages.success(request, 'This is About-1')
+    messages.success(request, 'This is About-2')
     return render(request, 'home/about.html')
-
 
 def contact(request):
     if request.method=='POST':
@@ -17,8 +22,12 @@ def contact(request):
         email = request.POST['email']
         phone = request.POST['phone']
         content = request.POST['content']
-        # print(name, email, phone, content)
         contact = Contact(name=name, email=email, phone=phone, content=content)
         contact.save()
-
+        if len(name) < 2 or len(email) < 3 or len(phone) < 10 or len(content) < 4:
+            messages.error(request, "Please fill the form correctly")
+        else:
+            contact = Contact(name=name, email=email, phone=phone, content=content)
+            contact.save()
+            messages.success(request, "Your message has been successfully sent")
     return render(request, 'home/contact.html')
